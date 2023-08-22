29fe4792c7d8b67a0aa615bd0ea24f59c555fbdf
randy hac <randydhacks@gmail.com>
2023-08-21 23:51:07 -0700

    changed hover duration for sidemenu

--- frontend/src/components/Application/SideMenu/SideMenu.js
+++ frontend/src/components/Application/SideMenu/SideMenu.js
@@ -14,7 +14,7 @@ function SideMenu() {
           <BiHomeAlt2 className="w-[20px] h-[20px] text-[#A2A0A2] mr-[10px]"/> <span className="text-[14px] font-medium">Home</span>
         </NavLink>
       </li>
-        <li className="list-none p-[8px] hover:bg-[#454547] rounded-[10px]">
+        <li className="list-none p-[8px] hover:bg-[#454547af] rounded-[10px] hover:ease-out duration-100">
         <NavLink to="/home" className="text-white flex items-center">
           <AiOutlineCheckCircle className="w-[20px] h-[20px] text-[#A2A0A2] mr-[10px]"/> <span className="text-[14px] font-medium">My Tasks</span>
         </NavLink>
