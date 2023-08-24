febbc7d6946be3d2fdd60c517763941a1d32fa18
randy hac <randydhacks@gmail.com>
2023-08-23 21:55:50 -0700

    added resizable text field based on inputs

---
+++ frontend/src/components/Application/Project/Board/CreateCard.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { GoCheckCircle } from "react-icons/go";
+import { useSelector } from "react-redux";
+
+function CreateCard(props) {
+
+    const {resize, i, section, setTitle, title } = props
+    const user = useSelector(state => state.session.user)
+
+  return (
+    <>
+      <form className="w-[280px] h-auto bg-white rounded-[8px] my-[5px] border-solid border-[1px] shadow-sm border-gray-400 hover:ease-out duration-200 cursor-pointer p-[10px]">
+        <div className="flex">
+          <span>
+            <GoCheckCircle className="text-[18px] w-[18px] h-[18px] mr-[5px] mt-[2px]" />
+          </span>
+          <span
+            className="textarea break-words max-w-[230px] max-h-[100px] outline-none w-[240px] inline-block"
+            role="textbox"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+              resize();
+            }}
+            contentEditable
+          ></span>
+        </div>
+        <div className="mt-[10px]">
+          {user.profileImage ? (
+            user.profileImage
+          ) : (
+            <div className="text-[10px] rounded-[50%] bg-yellow-300 h-[25px] w-[25px] flex items-center justify-center border-[1px] border-[#c3c3c3]">
+              {user.firstName[0].toUpperCase()}
+              {user.lastName[0].toUpperCase()}
+            </div>
+          )}
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default CreateCard;
--- frontend/src/components/Application/Project/Board/ProjectBoard.js
+++ frontend/src/components/Application/Project/Board/ProjectBoard.js
@@ -1,26 +1,48 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { BsThreeDots } from "react-icons/bs";
 import { getProjectSectionsThunk } from "../../../../store/sections";
 import { HiPlus } from "react-icons/hi";
-import { GoCheckCircle } from "react-icons/go";
+
+
+import "./ProjectBoard.css";
+import BoardCards from "./BoardCards";
+import CreateCard from "./CreateCard";
 
 function ProjectBoard() {
   const dispatch = useDispatch();
   const { id } = useParams();
   const sections = Object.values(useSelector((state) => state.sections));
 
-  const [addCard, setAddCard] = useState({ id: null, status: false })
+  const inside = useRef();
+  const outside = useRef(null);
 
+  const [addCard, setAddCard] = useState({ id: null, status: false });
+  const [title, setTitle] = useState("");
   useEffect(() => {
     dispatch(getProjectSectionsThunk(id));
   }, [id]);
 
-  console.log(sections);
+  // Dealing with Textarea Height
+  function calcHeight(value) {
+    const numberOfLineBreaks = (value.match(/\n/g) || []).length;
+    // min-height + lines x line-height + padding + border
+    const newHeight = 20 + numberOfLineBreaks * 20 + 12 + 2;
 
-  return (
+    if (newHeight < 100) return newHeight;
+  }
+  const resize = () => {
+      let textarea = document.querySelector(".textarea");
+
+      textarea.addEventListener("keyup", () => {
+        textarea.style.height = calcHeight(textarea.value) + "px";
+      });
+  }
+
+  return (sections &&
     <div className="pt-[20px] px-[10px] h-full flex bg-[#F9F8F8]">
+        {/* ---------------- SECTIONS MAPPING -------------------- */}
       {sections.map((section, i) => {
         return (
           <div
@@ -30,35 +52,23 @@ function ProjectBoard() {
             <div className="flex items-center justify-between p-[10px] w-full">
               <p className="font-medium text-[16px]">{section.name}</p>
               <div className="flex">
-                <HiPlus className="mr-[10px] cursor-pointer" onClick={e => setAddCard({id: section.id, status: !addCard.status})}/>
+                <HiPlus
+                  className="mr-[10px] cursor-pointer"
+                  onClick={(e) => {
+                    setAddCard({ id: section.id, status: !addCard.status });
+                    setTitle("");
+                  }}
+                />
                 <BsThreeDots className="cursor-pointer" />
               </div>
             </div>
             <div className="overflow-x-hidden overflow-y-scroll">
-                {addCard.status && section.id === addCard.id ? <form className="w-[280px] bg-white rounded-[8px] h-fit my-[5px] border-[#ECEAE9] border-solid border-[1px] shadow-sm hover:border-gray-400 hover:ease-out duration-200 cursor-pointer p-[10px]">
+              {addCard.status && section.id === addCard.id ? (
+                <CreateCard resize={resize} i={i} section={section} title={title} setTitle={setTitle}/>
+              ) : null}
 
-                    </form> : null}
-              {section.Cards.map((card, i) => {
-                return (
-                  <div key={i} className="w-[280px] bg-white rounded-[8px] h-fit my-[5px] border-[#ECEAE9] border-solid border-[1px] shadow-sm hover:border-gray-400 hover:ease-out duration-200 cursor-pointer p-[10px]">
-                    <span className="break-normal max-w-[270px] break-words flex">
-                      <span><GoCheckCircle className="text-[18px] w-[18px] h-[18px] mr-[5px] mt-[3px]" /></span>
-                      <span className="break-words max-w-[230px]">{card.title} dasdksjadlkasjdklasjdksaldjasdklasjdlksajdlsajdklajkldas</span>
-
-                    </span>
-                    <div className="mt-[5px]">
-                      {card.User.profileImage ? (
-                        card.User.profileImage
-                      ) : (
-                        <div className="text-[10px] rounded-[50%] bg-yellow-300 h-[25px] w-[25px] flex items-center justify-center border-[1px] border-[#c3c3c3]">
-                          {card.User.firstName[0].toUpperCase()}
-                          {card.User.lastName[0].toUpperCase()}
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                );
-              })}
+              {/* ---------------------------- CARDS MAPPING */}
+              <BoardCards section={section}/>
             </div>
           </div>
         );
