4ffe99ec2dc757cec8cea4e9265e8b0dc889ee28
randy hac <randydhacks@gmail.com>
2023-08-23 22:35:09 -0700

    added -webkit line clamp

--- frontend/src/components/Application/Project/Board/ProjectBoard.js
+++ frontend/src/components/Application/Project/Board/ProjectBoard.js
@@ -5,7 +5,6 @@ import { BsThreeDots } from "react-icons/bs";
 import { getProjectSectionsThunk } from "../../../../store/sections";
 import { HiPlus } from "react-icons/hi";
 
-
 import "./ProjectBoard.css";
 import BoardCards from "./BoardCards";
 import CreateCard from "./CreateCard";
@@ -33,47 +32,56 @@ function ProjectBoard() {
     if (newHeight < 100) return newHeight;
   }
   const resize = () => {
-      let textarea = document.querySelector(".textarea");
+    let textarea = document.querySelector(".textarea");
 
-      textarea.addEventListener("keyup", () => {
-        textarea.style.height = calcHeight(textarea.value) + "px";
-      });
-  }
+    textarea.addEventListener("keyup", () => {
+      textarea.style.height = calcHeight(textarea.value) + "px";
+    });
+  };
 
-  return (sections &&
-    <div className="pt-[20px] px-[10px] h-full flex bg-[#F9F8F8]">
+  return (
+    sections && (
+      <div className="pt-[20px] px-[10px] h-full flex bg-[#F9F8F8]">
         {/* ---------------- SECTIONS MAPPING -------------------- */}
-      {sections.map((section, i) => {
-        return (
-          <div
-            key={i}
-            className="h-full w-[300px] mx-[10px] rounded-[5px] flex flex-col items-center border-[#ECEAE9] border-solid border-[1px]"
-          >
-            <div className="flex items-center justify-between p-[10px] w-full">
-              <p className="font-medium text-[16px]">{section.name}</p>
-              <div className="flex">
-                <HiPlus
-                  className="mr-[10px] cursor-pointer"
-                  onClick={(e) => {
-                    setAddCard({ id: section.id, status: !addCard.status });
-                    setTitle("");
-                  }}
-                />
-                <BsThreeDots className="cursor-pointer" />
+        {sections.map((section, i) => {
+          return (
+            <div
+              key={i}
+              className="h-full w-[300px] mx-[10px] rounded-[5px] flex flex-col items-center border-[#ECEAE9] border-solid border-[1px]"
+            >
+              <div className="flex items-center justify-between p-[10px] w-full">
+                <p className="font-medium text-[16px]">{section.name}</p>
+                <div className="flex">
+                  <HiPlus
+                    className="mr-[10px] cursor-pointer"
+                    onClick={(e) => {
+                      setAddCard({ id: section.id, status: !addCard.status });
+                      setTitle("");
+                    }}
+                  />
+                  <BsThreeDots className="cursor-pointer" />
+                </div>
+              </div>
+              <div className="overflow-y-auto h-full overflow-x-hidden">
+                {/* ---------------------------- CREATE CARD ---------------------------- */}
+                {addCard.status && section.id === addCard.id ? (
+                  <CreateCard
+                    resize={resize}
+                    i={i}
+                    section={section}
+                    title={title}
+                    setTitle={setTitle}
+                  />
+                ) : null}
+
+                {/* ---------------------------- CARDS MAPPING ---------------------------- */}
+                <BoardCards section={section} />
               </div>
             </div>
-            <div className="overflow-y-auto h-full overflow-x-hidden">
-              {addCard.status && section.id === addCard.id ? (
-                <CreateCard resize={resize} i={i} section={section} title={title} setTitle={setTitle}/>
-              ) : null}
-
-              {/* ---------------------------- CARDS MAPPING */}
-              <BoardCards section={section}/>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    )
   );
 }
 
