34a4a74bff217b4810c78bf4a147fbdb86aa6ae1
randy hac <randydhacks@gmail.com>
2023-08-30 14:37:41 -0700

    added local storage for sidemenu closing

--- frontend/src/components/Application/Navigation/AppNavigation.js
+++ frontend/src/components/Application/Navigation/AppNavigation.js
@@ -1,22 +1,18 @@
-import { RiMenuLine } from 'react-icons/ri'
+import { RiMenuLine } from "react-icons/ri";
 import { useContext, useEffect } from "react";
 import { ModalContext } from "../../../context/Modal";
 import { NavLink, useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logout } from "../../../store/session";
 import ProfileIcon from "../Profile/ProfileIcon";
+import { reactLocalStorage } from "reactjs-localstorage";
 
 function AppNavigation(props) {
   const dispatch = useDispatch();
   const history = useHistory();
-  const { createProjectModal, type } = useContext(ModalContext);
   const { setCloseSideMenu, closeSideMenu } = props;
 
-  const logoutHandler = async (e) => {
-    e.preventDefault();
-    await dispatch(logout());
-    return history.push("/login");
-  };
+
 
   return (
     <section className="bg-[#2E2E30] h-[50px] flex items-center justify-between border-solid border-[#424244] border-b-[1px]">
@@ -24,12 +20,18 @@ function AppNavigation(props) {
         {closeSideMenu ? (
           <RiMenuLine
             className="text-[#f5f4f3] h-[20px] w-[20px] cursor-pointer "
-            onClick={(e) => setCloseSideMenu(!closeSideMenu)}
+            onClick={(e) => {
+              setCloseSideMenu(!closeSideMenu);
+              reactLocalStorage.set("sideMenu", closeSideMenu);
+            }}
           />
         ) : (
           <RiMenuLine
             className="text-[#f5f4f3] h-[20px] w-[20px] cursor-pointer"
-            onClick={(e) => setCloseSideMenu(!closeSideMenu)}
+            onClick={(e) => {
+              setCloseSideMenu(!closeSideMenu);
+              reactLocalStorage.set('sideMenu', closeSideMenu)
+            }}
           />
         )}
         <NavLink
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AppNavigation from "./Application/Navigation/AppNavigation";
 import SideMenu from "./Application/SideMenu/SideMenu";
 import AppHome from "./Application/AppHome/AppHome";
@@ -6,13 +6,24 @@ import ProjectOverviewPage from "./Application/Project/ProjectOverviewPage";
 import Modal from "./utils/Modal";
 import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
-import ProjectBoard from "./Application/Project/Board/ProjectBoard";
+import {reactLocalStorage} from 'reactjs-localstorage';
+
 
 function Main(props) {
   const [closeSideMenu, setCloseSideMenu] = useState(true);
   const { compType } = props;
 
   const userSession = useSelector((state) => state.session.user);
+
+
+  useEffect(() => {
+    const data = window.localStorage.getItem('SET_SIDE_MENU')
+    setCloseSideMenu(JSON.parse(data))
+  }, [])
+
+  useEffect(() => {
+    window.localStorage.setItem('SET_SIDE_MENU', JSON.stringify(closeSideMenu))
+  }, [closeSideMenu])
 
   if (!userSession) return <Redirect to="/login" />;
 
