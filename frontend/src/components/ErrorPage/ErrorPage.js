bbc3117f715f959a02969df7d627070390a56364
randy hac <randydhacks@gmail.com>
2023-08-30 21:06:19 -0700

    fixing issue with blank page when loading

--- frontend/src/components/ErrorPage/ErrorPage.js
+++ frontend/src/components/ErrorPage/ErrorPage.js
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 
 
 function ErrorPage() {
+
+    const userSession = useSelector(state => state.session.user)
+
+    if (!userSession) return <Redirect to='/login'/>
+
     return (
         <div>ERROR 404</div>
     )
