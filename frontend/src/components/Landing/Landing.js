6ab26a745cd234242a76a47cbd17d728e602db0c
randy hac <randydhacks@gmail.com>
2023-09-06 18:22:07 -0700

    fix scrolling issue for overflow

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -14,15 +14,25 @@ function Landing() {
             phoebe
           </NavLink>
           <div className="flex text-[16px] items-center">
-            <div className="mr-[20px] text-[#727272] font-medium">Login</div>
-            <div className="bg-black text-white h-[36px] w-[116px] rounded-[5px] flex items-center justify-center">Get Started</div>
+            <NavLink to='/login' className="mr-[20px] text-[#727272] font-normal hover:text-black duration-200 ease-in">Login</NavLink>
+            <NavLink to='/signup' className="bg-black text-white h-[36px] w-[116px] rounded-[5px] flex items-center justify-center">
+              Get Started
+            </NavLink>
           </div>
         </div>
         <div className="flex items-center flex-col my-auto w-full justify-center">
-            <div className="w-full items-center flex justify-center flex-col">
-            <h1 className="text-[52px] w-[480px] text-center leading-[50px] font-light">Productivity platform for task management</h1>
-            <p>Want more efficiency in your organization? Phoebe is easy for all teams to use, so you can deliver quality work, faster.</p>
+          <div className="w-full items-center flex justify-center flex-col h-[200px] ">
+            <div className="flex flex-col text-center justify-evenly h-full w-[600px]">
+              <h1 className="text-[52px] w-full text-center leading-[50px] font-light tracking-wider h-full flex justify-center items-center">
+                Productivity platform for task management
+              </h1>
+              <p className="text-center text-[18px] tracking-wide w-full">
+                Want more efficiency in your organization? Phoebe is easy for
+                all teams and personal use, so you can deliver quality work,
+                faster.
+              </p>
             </div>
+          </div>
         </div>
       </div>
     </div>
